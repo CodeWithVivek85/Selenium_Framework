5c571b539c371726e45f3a63138848db8f94d478
Hp <Hp@DESKTOP-BMU1KM3>
2024-06-03 14:20:49 +0530

    Framework Uploaded

---
+++ SampleSelenium/src/test/java/prac2/OrgTest.java
@@ -0,0 +1,19 @@
+package prac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+		@Test
+		public void createOrg()
+		{
+			System.out.println("execute query");
+		}
+		@Test
+		public void modifyOrg()
+		{
+			System.out.println("execute modifyOrgTest");
+		}
+
+
+}
